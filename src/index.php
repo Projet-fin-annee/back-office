1aaec4e0c80e1c9a515b4656b01fc5a6c1460006
tania-abitbol <tabitbol@hotmail.com>
2020-06-08 12:37:39 +0200

    creates all routes

--- src/index.php
+++ src/index.php
@@ -1,4 +1,5 @@
 <?php
+
 define("URL", str_replace("index.php", "", (isset($_SERVER["HTTPS"]) ? "https" : "http") . "://$_SERVER[HTTP_HOST]"));
 session_start();
 require_once('controllers/Router.php');
