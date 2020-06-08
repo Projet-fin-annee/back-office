e7205067bd30774a10818f9cca612b0a24f5c487
Abitbol Tania <56915461+tania-abitbol@users.noreply.github.com>
2020-06-08 15:22:27 +0200

    Routes (#4)

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
+
 define("URL", str_replace("index.php", "", (isset($_SERVER["HTTPS"]) ? "https" : "http") . "://$_SERVER[HTTP_HOST]"));
 session_start();
 require_once('controllers/Router.php');
 
 $router = new Router();
 $router->routeReq();
+
 
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
