02108732080141fa35c9f6dd7c50683fcedcf106
tania-abitbol <tabitbol@hotmail.com>
2020-06-11 20:23:08 +0200

    add database

---
+++ data/webdoc.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jun 11, 2020 at 06:20 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
+
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone
+= "+00:00";
+
+--
+-- Database: `webdoc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `countries`
+--
+
+CREATE TABLE `countries`
+(
+  `id` int
+(100) NOT NULL,
+  `country` varchar
+(15) NOT NULL,
+  `title` varchar
+(150) DEFAULT NULL,
+  `text` text,
+  `image` varchar
+(1000) DEFAULT NULL,
+  `video` varchar
+(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `countries`
+--
+
+INSERT INTO `countries` (`
+id`,
+`country
+`, `title`, `text`, `image`, `video`) VALUES
+(3, 'france', 'la france lala', 'lsdl,fiejufjkjejfioerjfgiverfp\r\nvdujfuiejg', 'Capture d’écran 2020-06-04 à 16.35.20.png', ''),
+(4, 'espagne', 'jdjj', 'djjdj', 'jddj', 'djdj'),
+(7, 'eshpagne', 'jdhjj', 'djjdj', 'jddj', 'djdj'),
+(8, '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `definition`
+--
+
+CREATE TABLE `definition`
+(
+  `id` int
+(255) NOT NULL,
+  `word` varchar
+(100) DEFAULT NULL,
+  `text` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `definition`
+--
+
+INSERT INTO `definition` (`
+id`,
+`word
+`, `text`) VALUES
+(1, '', ''),
+(2, 'ff', 'ff'),
+(3, '', ''),
+(4, '', ''),
+(5, '', ''),
+(6, '', ''),
+(7, '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users`
+(
+  `id` int
+(255) NOT NULL,
+  `email` varchar
+(100) NOT NULL,
+  `password` varchar
+(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`
+id`,
+`email
+`, `password`) VALUES
+(1, 'tania.abitbol@hetic.net', 'webdoc2020xmp'),
+(2, 'nathan.stampfli@hetic.net', 'webdoc2020xmp'),
+(3, 'maeva.wolff@hetic.net', 'webdoc2020xmp'),
+(4, 'lina.blidi@hetic.net', 'webdoc2020xmp'),
+(5, 'bastien.calou@gmail.com', 'webdoc2020xmp'),
+(6, 'aymericmayeux@gmail.com\r\n', 'webdoc2020xmp'),
+(7, 'laura.fraysse@hetic.net', 'webdoc2020xmp');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `countries`
+--
+ALTER TABLE `countries`
+ADD PRIMARY KEY
+(`id`),
+ADD UNIQUE KEY `id`
+(`id`),
+ADD UNIQUE KEY `country`
+(`country`),
+ADD UNIQUE KEY `title`
+(`title`);
+
+--
+-- Indexes for table `definition`
+--
+ALTER TABLE `definition`
+ADD PRIMARY KEY
+(`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ADD PRIMARY KEY
+(`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int
+(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `definition`
+--
+ALTER TABLE `definition`
+  MODIFY `id` int
+(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int
+(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
